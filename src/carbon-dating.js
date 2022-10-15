0675a00279cecea8931a7dd245af461cf83a7e49
Danil Kuznetsov <wzrs45@gmail.com>
2022-10-15 19:36:06 +0700

    feat: resolve "carbon dating" task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  let num = Number(sampleActivity);
+  if ((num>15) || (num<=0) || isNaN(num)) return false;
+  let ln = Math.log(MODERN_ACTIVITY / num);
+  return Math.ceil(ln / (0.693 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
